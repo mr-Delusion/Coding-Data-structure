fdacc06cc7e8544117298e627ece3f35d711997c
mr-Delusion <franciskmachua@gmail.com>
2024-10-12 14:28:20 +0300

    File handling

--- File handling/File handling.py
+++ File handling/File handling.py
@@ -1,3 +1,14 @@
-#File Handling with differnt access modes
-file = open("File handling\Sample_File.txt")
-print(file.read())
+#remove comment marks to test
+
+# file = open("File handling\Sample_File.txt")
+# print(file.read())
+# file.close()
+
+# file = open("File handling\Sample_File.txt", "w")
+# file.write("Coding is the best skill in the world!")
+# file.close()
+
+# file = open("File handling\Sample_File.txt", "a")
+# file.write("Coding is the best skill in the world!")
+# file.close()
+
